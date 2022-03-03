3040543d42528377230ad67ef031f18572484394
Maria Eduarda Casalunga <mariaeduardacasalunga@gmail.com>
2022-03-02 21:59:20 -0300

    Finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Diz pra Mim");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Diz pra mim que hoje não dá pé");
+Console.WriteLine("Que as nuvens não dormem");
+Console.WriteLine("Enquanto eu me afogo em você");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Baby não viaja em mim");
+Console.WriteLine("Você disse que eu não sou assim");
+Console.WriteLine("Mas lembro quando nada dava pé");
+Console.WriteLine("Mais eu consegui nadar e fugi");
+Console.WriteLine("Baby eu não estou nada bem");
+Console.WriteLine("Falei tanta besteira e me tornei refém");
+Console.WriteLine("Mas lembro quando nada dava pé");
+Console.WriteLine("Você se contradizia e eu também");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Você sempre me aponta");
+Console.WriteLine("Que quando eu discordo ");
+Console.WriteLine("Lembro que eu estou na ponta");
+Console.WriteLine("Que sempre que eu me deito");
+Console.WriteLine("Volta aquela pergunta");
+Console.WriteLine("Se eu quero a liberdade");
+Console.WriteLine("Ou ter você só pra mim ");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Não foi normal");
+Console.WriteLine("Não fiz por mal");
+Console.WriteLine("Sem desastres");
+Console.WriteLine("A vida é tão igual");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Diz pra mim que hoje não dá pé ");
+Console.WriteLine("Que as nuvens não dormem");
+Console.WriteLine("Enquanto eu me afogo em você");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("A noite cai e o frio abraça quem vê");
+Console.WriteLine("Não deixei de cantar");
+Console.WriteLine("Enquanto a vida me der corda");
+Console.WriteLine("Não tem ordem de parar");
+Console.WriteLine("Não me permitirei parar");
+Console.WriteLine("Me diga");
+Console.WriteLine("Porque seu jeito não passa");
+Console.WriteLine("Pelos meus sentidos");
+Console.WriteLine("De desejar você");
+Console.WriteLine("Tão bom quando tempo passa de pressa");
+Console.WriteLine("Sabia que um dia talvez nem pensasse mais nessa");
+Console.WriteLine("Vez que o dia foi bom pra mim e pra você");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Baby não viaja em mim");
+Console.WriteLine("Você disse que eu não sou assim ");
+Console.WriteLine("Mas lembro quando nada dava pé ");
+Console.WriteLine("Mais eu consegui nadar e fugi");
+Console.WriteLine("Baby eu não estou nada bem");
+Console.WriteLine("Falei tanta besteira e me tornei refém");
+Console.WriteLine("Mas lembro quando nada dava pé");
+Console.WriteLine("Você se contradizia e eu também");
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Diz pra mim que hoje não dá pé");
+Console.WriteLine("Que as nuvens não dormem");
+Console.WriteLine("Enquanto eu me afogo em você");
+Console.WriteLine("");
+Console.ResetColor();
+Console.ReadKey();
+Console.Clear();
